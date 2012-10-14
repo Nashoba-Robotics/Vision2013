acffa1a98a09f2c424c1bf749c9a540f94f3a1e1
Alex <alex_brinister@yahoo.com>
2012-10-14 10:59:54 -0400

    Added a header file and a license

--- Coprocessor/VisionNew2.cxx
+++ Coprocessor/VisionNew2.cxx
@@ -1,188 +1,4 @@
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include <iostream>
-#include <sstream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <getopt.h>
-
-#include <unistd.h>
-#include <stdio.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <resolv.h>
-#include <arpa/inet.h>
-
-#define DEBUG_TEXT      1
-#define BUFFERSIZE		1024
-
-#define CRIO_NETWORK               // Normal case
-//#define WPI_IMAGES               // For debugging with WPI images
-
-using namespace cv;
-using namespace std;
-
-// Image Color Plane definitions
-int BLUE_PLANE  = 0;
-int GREEN_PLANE = 1;
-int RED_PLANE   = 2;
-
-Mat src;                            // The source image
-bool pause_image = false;
-int thresh = 130;                   // Defines the threshold level to apply to image
-int thresh_block_size = 23;         // Defines the threshold block size to apply to image
-int max_thresh_block_size = 255;    // Defines the threshold block size to apply to image
-int max_thresh = 255;               // Max threshold for the trackbar
-int poly_epsilon = 10;              // Epsilon to determine reduce number of poly sides
-int max_poly_epsilon = 50;          // Max epsilon for the trackbar
-
-int minsize = 500;                  // Polygons below this size are rejected
-int max_minsize = 10000;            // Max polygon size for the trackbar
-
-int dilation_elem = 0;              // The element type dilate/erode with
-int const max_elem = 2;             // Max trackbar element type
-int dilation_size = 4;              // The size of the element to dilate/erode with
-int const max_kernel_size = 21;     // Max trackbar kernel dialation size
-
-int erode_count = 1;                // The number of times to erode the image
-int erode_max = 20;                 // Max number of times to erode on trackbar
-
-int target_width_inches = 24;       // Width of a physical target in inches
-int target_height_inches = 16;      // Height of a physical target in inches
-
-/* The command line options processing class
- * Processes command line options using getopt_long()
- */ 
-class OptionsProcess 
-{
-    public:
-        int processCamera;
-        int guiAll;
-        int verbose_flag;
-        int processVideoFile;
-        int processJpegFile;
-        char *fileName;
-
-        OptionsProcess(): 
-            processCamera(true), 
-            guiAll(false), 
-            processVideoFile(false),
-		    processJpegFile(false),
-		    fileName(0) 
-            {}
-
-        int processArgs(int argc, char** argv)
-        {
-            int get_longOptions;
-    
-             while (1) 
-             {
-                /* This struct contains all the command line arguments
-                 * we want to filter. So far, we have arguments for our debug
-                 * mode.
-                 */ 
-                static struct option long_options[] =
-	            {
-	                /* These options set a flag */
-	                {"verbose",     no_argument,       &verbose_flag, 'v'},     // The verbosity flag
-	                {"guiAll",      no_argument,       &guiAll, 'g'},           // The debug flag showing all of the windows
-	                {"brief",       no_argument,       &verbose_flag, 'b'},     // The anti-verbosity flag
-	                
-                    /* These options don't set a flag.
-	                We distinguish them by their indices */
-	                {"help",        no_argument,        0, 'h'},                // The help flag
-	                {"wpiImages",   no_argument,        0, 'w'},                // The WPI image processing flag
-	                {"file",        required_argument,  0, 'f'},                // The jpeg file loading flag
-	                {0, 0, 0, 0}                                                // The default, no options flag
-	            };
-      
-                /* getopt_long stores the option index here. */
-                int option_index = 0;
-      
-                get_longOptions = getopt_long (argc, argv, "f:h", long_options, &option_index);
-      
-                /* Detect the end of the options. */
-                if (get_longOptions == -1) break;
-      
-                 switch (get_longOptions) 
-                 {
-                    case 0:
-	                /* If this option sets a flag, do nothing else now */
-	                    if (long_options[option_index].flag != 0)    break;
-
-	                    printf ("option %s", long_options[option_index].name);
-
-	                    if (optarg)    printf (" with arg %s", optarg);
-
-	                    printf ("\n");  
-                        
-                        break;
-	
-                    case 'f': 
-                    {
-	                    string optstring(optarg);
-
-                        // Find ".jpg" in the argument string
-	                    int jpeg =  optstring.find(".jpg");
-
-                        /* If ".jpg" is found in the file name,
-                         * set the processJpegFile boolean to true
-                         * and the processVideoFile and processCamera
-                         * to booleans to false
-                         */ 
-                        if (jpeg != optstring.npos)
-                        {
-	                        processJpegFile = true;
-	                        processVideoFile = false;
-	                        processCamera = false;
-	                    } 
-                        else // Else set the opposite
-                        {
-	                        processJpegFile = false;
-	                        processVideoFile = true;
-                            processCamera = true;
-	                    }
-	                    
-                        // File name is equal to the file name inputted 
-                        fileName = optarg;
-	                    break;
-                    }
-                    
-                    case 'w':
-	                
-                    // Flop the green and red planes
-	                BLUE_PLANE = 0;
-	                GREEN_PLANE = 2;
-	                RED_PLANE = 1;
-	                break;
-      
-                    case 'h':
-                    
-                    // Print the help menu
-	                printf("Usage: ./Vision\n"); 
-                    printf("[-h|--help]:\tPrint this message\n");
-	                printf("[--guiAll]:\tDisplay all debugging windows\n");
-	                printf("[-w|--wpiImages]:\tProcess WPI type images (red targets)\n");
-	                printf("[-f|--file] filename : Process a mjpg video or jpeg image\n");
-	                
-                    exit(0);
-	                break;
-	
-                    case '?':
-	                
-                    /* getopt_long already printed an error message. */
-	                break;
-	
-                    default: // The default action is to exit
-	                abort ();
-                 }
-             }
-        }
-};
-
-OptionsProcess options;
+#include "./Vision.h"
 
 // A timer using the timespec struct
 timespec diff(timespec start, timespec end)
@@ -676,14 +492,15 @@ void getTargetData(Mat &image, const vector<vector<Point2f> >&targetQuads, vecto
         target.sizeY = sizeY;
 
         /* Distance to target was obtained from distance to target measurements
-         * that were trend line fit in Excel
+         * that were trend line fit in LibreOffice
          */ 
         target.distanceX = 11263 * pow(sizeX, -1.061);
         target.distanceY = 7239 * pow(sizeY, -1.025);
     
         // Angle per pixel is based on the camera perameters
         target.angleX = 0.160943017 * ((image.cols / 2) - target.centerX) + 2.3;
-        computeTargetType(target);
+        
+	computeTargetType(target);
 
         targets.push_back(target);
     }
